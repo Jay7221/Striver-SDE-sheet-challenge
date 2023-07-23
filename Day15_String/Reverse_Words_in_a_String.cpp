3cbd2db825d3cd156aed4bfd4628b0a83c460dea
Jay7221 <jns29072003@gmail.com>
2023-07-23 16:33:23 +0530

    Modified Day 15 reverse words in a string

--- Day15_String/Reverse_Words_in_a_String.cpp
+++ Day15_String/Reverse_Words_in_a_String.cpp
@@ -1,28 +1,25 @@
 string reverseString(string &s){
-	while(!s.empty() && (s.back() == ' ')){
-		s.pop_back();
-	}
-	reverse(s.begin(), s.end());
-	while(!s.empty() && (s.back() == ' ')){
-		s.pop_back();
-	}
-	int ind = 0;
-	for(int i = 0; i < s.size(); ++i){
-		if((i > 0) && (s[i] == s[i - 1]) && (s[i] == ' ')){
-			continue;
-		}
-		s[ind] = s[i];
-		++ind;
-	}
-	int n = ind;
-	s.resize(n);
-	int l = 0;
-	for(int r = 0; r < n; ++r){
-		if(s[r] == ' '){
-			reverse(s.begin() + l, s.begin() + r);
-			l = r + 1;
+	string curWord;
+	vector<string> words;
+	s.push_back(' ');
+	for(char ch : s){
+		if(ch == ' '){
+			if(!curWord.empty()){
+				words.push_back(curWord);
+				curWord.clear();
+			}
+		}else{
+			curWord.push_back(ch);
 		}
 	}
-	reverse(s.begin() + l, s.begin() + n);
-	return s;
+	reverse(words.begin(), words.end());
+	string ans;
+	for(string word : words){
+		ans += word;
+		ans.push_back(' ');
+	}
+	if(!ans.empty()){
+		ans.pop_back();
+	}
+	return ans;
 }
